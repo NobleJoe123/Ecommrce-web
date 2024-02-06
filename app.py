154ce53c301d502ba342f2447d25d829827a6ab3
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-02-06 06:33:40 +0100

    add

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+
+app.secret_key = 'key'
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'advance'
+
+
+mysql = MySQL(app)
